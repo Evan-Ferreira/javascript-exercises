a768ca3cb8811ebb99d856b825ee395ae4704b40
Evan Ferreira <evanjfer@gmail.com>
2024-12-12 18:18:47 -0500

    Finish exercise 05 in javascript exercises
    - Complete sumAll.js and sumAll() function as per
    the requirements in the Odin Project

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function (n1, n2) {
+    if (n1 < 0 || n2 < 0) {
+        throw new Error();
+    }
+    let result = n1;
+    n1++;
+    for (n1; n1 <= n2; n1++) {
+        result += n1;
+    }
+    return result;
 };
 
 // Do not edit below this line
