7fffbcfa82aa8ebd43daa66363bd48b6e6579e12
Evan Ferreira <evanjfer@gmail.com>
2024-12-12 18:01:13 -0500

    Finish javascript exercises 01, 02, and 03
    - Completed reverseString.js, repeatString.js, and
    helloWorld.js exercises as per theodinproject.com

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function () {
+    if (n < 0) {
+        throw new error();
+    }
+    let result = '';
+    for (let i = 0; i < n; i++) {
+        result += s;
+    }
+    return result;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function () {
+    let result = '';
+    for (let i = s.length - 1; i > -1; i--) {
+        result += s[i];
+    }
+    return result;
 };
 
 // Do not edit below this line
