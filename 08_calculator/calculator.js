043c63aefc39836fa3c5a5fda5a0f65354932211
Evan Ferreira <evanjfer@gmail.com>
2024-12-14 14:32:54 -0500

    Finish calculator exercise 08 per TOP
    - Complete Add, Subtract, Multiply, Divide functions,
    all per TOP instructions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,39 @@
-const add = function() {
-	
+const add = function (a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+    return arr.reduce((total, curr) => {
+        return total + curr;
+    }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (a, b) {
+    return a * b;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+    return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+    const result = 1;
+    for (let i = a; i > 0; i--) {
+        result * i;
+    }
+    return result;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial,
 };
