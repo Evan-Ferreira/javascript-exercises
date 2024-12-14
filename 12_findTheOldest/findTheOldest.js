4f3abf2d66280a2f78e5565a96c4c3d9a9efb5a3
Evan Ferreira <evanjfer@gmail.com>
2024-12-14 15:13:24 -0500

    Finish javascript exercise 12 of TOP
    - Completed as per the instructions of TOP

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+    return arr.reduce((oldest, person) => {
+        if (!person.yearOfDeath) {
+            person.yearOfdeath = new Date().getFullYear();
+        }
+        const perAge = person.yearOfDeath - person.yearOfBirth;
+        const oldAge = oldest.yearOfDeath - person.yearOfBirth;
+        return oldAge < perAge ? person : oldest;
+    });
 };
 
 // Do not edit below this line
