d7ac6f6fdfcf1a14a5e23921b39cbd813823cacd
Evan Ferreira <evanjfer@gmail.com>
2024-12-14 14:49:37 -0500

    Finish javascript exercise 10 of TOP
    - Finish exercise 10 as specified by isntructions
    of TOP

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function (s) {
+    if (s === 0) {
+        return 0;
+    } else if (s === 1) {
+        return 1;
+    } else {
+        return fibonacci(s - 1) + fibonacci(s - 2);
+    }
 };
 
 // Do not edit below this line
